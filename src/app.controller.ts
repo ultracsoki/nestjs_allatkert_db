503b6810a843382361969088508ce877a5d5fed5
mac <halasi.czalbert@petrik.hu>
2023-12-01 12:53:34 +0100

    Feladat elkészítése (bootstrap hiányzik)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as mysql from 'mysql2';
+import { ujAllatDto } from './ujAllatDTO';
+import e, { Response } from 'express';
+
+const conn = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'allatkert',
+}).promise();
 
 @Controller()
 export class AppController {
@@ -7,7 +17,40 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index() {
+    const [ adatok ] = await conn.execute('SELECT id, nev, eletkor, fajta FROM allatok');
+    console.log(adatok);
+
+    return {
+       allatok: adatok, 
+      };
+  }
+
+
+  @Get('/ujAllat')
+  @Render('ujAllat')
+  ujAllatForm() {
+    return {messages: ''};
+  }
+
+  @Post('/ujAllat')
+  @Render('ujAllat')
+  async ujAllat(@Body() ujAllat: ujAllatDto, @Res() res: Response) {
+    const nev = ujAllat.nev;
+    const eletkor = ujAllat.eletkor;
+    const fajta = ujAllat.fajta;
+    if(nev == "" || eletkor == "" || fajta == "") {
+      return { messages: "Minden mezőt kötelező kitölteni!"};
+    } else if (parseInt(eletkor) < 0){
+      return { messages: "Az életkor nem lehet negatív!"};
+    } else {
+      const [ adatok ] = await conn.execute('INSERT INTO allatok (nev, eletkor, fajta) VALUES (?, ?, ?)', [ 
+        nev,
+        eletkor,
+        fajta,
+      ],
+      );
+      res.redirect('/');
+    }
   }
 }
---
+++ src/ujAllatDTO.ts
@@ -0,0 +1,6 @@
+export class ujAllatDto {
+    nev: string;
+    eletkor: string;
+    fajta: string;
+  }
+  
